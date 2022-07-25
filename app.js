ab18e9d584e374662997b3a1016be70b1dd0ea87
Casey <orangobang@gmail.com>
2022-07-25 13:47:19 -0400

    Book object Creation added

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+//this is a basic constructor for creating a libary of books!
+const div = document.querySelector('.container');
+const library = []
+const newDiv = document.createElement('div')
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBook() {
+	let book = new Book();
+    book.title = document.getElementById('title').value;
+    book.author = document.getElementById('author').value;
+    book.pages = document.getElementById('pages').value;
+
+    console.log(book)
+    newDiv.innerHTML += `<h1>${book.title}</h1> <p> ${book.author}, ${book.pages}</p>`
+    div.append(newDiv)
+}
