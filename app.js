9fa0356b26a307f1fc60b601ea4933f0dfad6b37
Casey <orangobang@gmail.com>
2022-07-27 14:57:25 -0400

    some refactoring removebook not working yet

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
 const form = document.getElementById('myForm');
 const container = document.querySelector('.container');
 const library = document.querySelector('.library');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const removeBtn = document.getElementById('removeBtn');
 let bookId = 0;
-const books = [];
+const books = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R Tolkien',
+        pages: '304',
+        id: 0
+    },
+];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = bookId;
 }
 
 function addBook() {
-	let book = new Book();
-    book.title = document.getElementById('title').value;
-    book.author = document.getElementById('author').value;
-    book.pages = document.getElementById('pages').value;
+	let book = new Book(title.value, author.value, pages.value);
+    books.push(book);
+    console.log(books);
+    //revert page style after subbmitting book info
+    form.style.visibility = 'hidden';
+    container.style.opacity = '1';
+    container.style.backgroundColor = 'rgb(180, 25, 46)';
+    render(book);
+    clearForm();
+}
 
+function removeBook() {
+    let pickedBookId = parseInt(removeBtn.dataset.id);
+    console.log(pickedBookId);
+    books.splice(pickedBookId, 1)
+    console.log(books);
+}
+
+function render(book) {
     let card = document.createElement('div');
     card.className = 'card';
     bookId += 1;
     card.dataset.id = bookId;
+    book.id = bookId;
 
     //Create card content with user input
     let cardContent = document.createElement('div');
     cardContent.className = 'cardContent';
-    cardContent.innerHTML = `<h1> ${book.title} </h1><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p>`
+    cardContent.innerHTML = `<h1> ${book.title} </h1>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    Read: <input id="read" type="checkbox" checked><br>
+    <button id='removeBtn' data-id='${bookId}' onclick='removeBook()'>Delete</button>`
+
     //Update DOM and return CSS style
     card.appendChild(cardContent);
     library.append(card);
-    books.push(card);
-    console.log(books)
-    form.style.visibility = 'hidden';
-    container.style.opacity = '1';
-    container.style.backgroundColor = 'rgb(180, 25, 46)';
-
 }
 
-function removeBook() {
-    
+function clearForm() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
 }
 
 function openForm() {
@@ -50,4 +78,5 @@ function closeForm() {
     form.style.visibility = 'hidden';
     container.style.opacity = '1';
     container.style.backgroundColor = 'rgb(180, 25, 46)';
+    clearForm();
 }
