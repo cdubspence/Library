011ef1bae99b7cb30e105cb972adf4e3c96ec48a
Casey <orangobang@gmail.com>
2022-07-28 22:09:10 -0400

    new index working

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
 const library = [];
-const container = document.querySelector('#container');
+const lib = document.querySelector('.library');
+const container = document.querySelector('.container');
+const form = document.getElementById('myForm');
 let bookId = 0;
 //const card = document.createElement('div');
 
-function Book(title, author, id) {
+function Book(title, author, pages) {
 	this.title = title;
-  this.author = author;
-  this.id = id;
+    this.author = author;
+    this.pages = pages;
 }
 
 function getBook() {
 	let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-  let id = bookId;
-  bookId++;
-  let book = new Book(title, author, id);
-  library.push(book);
-  console.log(library);
-  addBook();
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let id = bookId;
+    bookId++;
+    let book = new Book(title, author, pages);
+    library.push(book);
+    console.log(library);
+    addBook();
 }
 
 function addBook() {
-container.innerHTML = '';
-	library.forEach(book => {
-  	const card = document.createElement('div');
-    card.setAttribute('name', book.title);
-		card.innerHTML = `<h1>${book.title}</h1>
-		<p>Author: ${book.author}</p>
-    <button id='deleteBtn' onclick='deleteBook(this)'>Delete</button>`;
-    card.className = 'bookCard';
-    card.setAttribute('id', book.id)
-    container.append(card);
-	})
-  
+    lib.innerHTML = '';
+        library.forEach(book => {
+        const card = document.createElement('div');
+        card.setAttribute('name', book.title);
+            card.innerHTML = `<h1>${book.title}</h1>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+        <button id='deleteBtn' onclick='deleteBook(this)'>Delete</button>`;
+        card.className = 'bookCard';
+        card.setAttribute('id', book.id)
+        lib.append(card);
+        })
+    closeForm()
 }
 
 function deleteBook(element) {
 	let bookToBeRemoved = 
-  	library.find(book => book.title == element.parentNode.name);
-   library.splice(bookToBeRemoved, 1);
+  	    library.find(book => book.title == element.parentNode.name);
+    library.splice(bookToBeRemoved, 1);
 	element.parentNode.remove();
   
+}
+
+function clearForm() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+}
+
+function openForm() {
+    form.style.visibility = 'visible';
+    container.style.backgroundColor = 'rgb(180, 25, 46)';
+    container.style.opacity = '.7';
+}
+
+function closeForm() {
+    form.style.visibility = 'hidden';
+    container.style.opacity = '1';
+    container.style.backgroundColor = 'rgb(180, 25, 46)';
+    clearForm();
 }
